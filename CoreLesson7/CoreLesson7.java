9946f5ed3691b2277ebeabf0b802bacc707848c6
Ek-Ps <88617560+Ek-Ps@users.noreply.github.com>
2022-04-27 22:03:41 +0300

    Delete CoreLesson7 directory

--- CoreLesson7/CoreLesson7.java
+++
@@ -1,7 +0,0 @@
-package CoreLesson7;
-// ctrl + alt + L - форматировать код
-//https://javascopes.com/jackson-annotations-7a1a88eb/
-//https://javascopes.com/jackson-json-node-tree-model-3144aa91/
-
-public class CoreLesson7 {
-}
